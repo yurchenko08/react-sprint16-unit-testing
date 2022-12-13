6e302250f4f6b624773de0530b3f48ed636013a9
Sheludko Oleg <sheludko.c.s@gmail.com>
2022-12-13 12:28:26 +0200

    create unit test for App component

--- src/tests/AppComponent.test.js
+++ src/tests/AppComponent.test.js
@@ -1,7 +1,19 @@
-describe("just fake tests for App Component", () => {
-  it("1st fake test of App Component", () => {
-    expect(true).toEqual(false);
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "../components/App";
+
+describe("Test component App", () => {
+  let componentApp;
+  beforeEach(() => {
+    componentApp = renderer.create(<App />);
+  });
+
+  it("Render corectly", () => {
+    expect(componentApp.toJSON()).toMatchSnapshot();
+  });
+
+  it("Check type", () => {
+    expect(componentApp.toJSON().type).toBe("div");
   });
 });
-
-// TODO: Your test need to be here instead of fake tests
+// // TODO: Your test need to be here instead of fake tests
